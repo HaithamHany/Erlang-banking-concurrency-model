9ea4a2544fa9a9ab89ed36ae43d57e5078d18918
Haitham Abdel-Salam <haithamhany7@gmail.com>
2023-06-20 16:38:16 -0400

    Making a request from customer

--- src/customer.erl
+++ src/customer.erl
@@ -2,23 +2,18 @@
 -export([process_customer/4]).
 
 process_customer(MasterPID, Name, LoanNeeded, BankInfo) ->
-  BankIDs = lists:map(fun({BankName, _}) -> {BankName, whereis(BankName)} end, BankInfo),
+  BanksNamesWithIDs = lists:map(fun({BankName, _}) -> {BankName, whereis(BankName)} end, BankInfo),
   %io:fwrite("~s knows about ~p IDs~n", [Name, BankIDs]),
 
   %Random selection of loan and bank
-  RandomLoan = generate_random_loan(),
-  io:fwrite("Customer ~s requesting a loan of ~B dollars.~n", [Name, RandomLoan]),
-  RandomBank = select_random_bank(BankIDs),
-  io:fwrite("Customer ~s targeting bank: ~p~n", [Name, RandomBank]),
-
+  make_request(Name, BanksNamesWithIDs),
 
   %Message formulation Send/Recive
   Msg = {Name, LoanNeeded, BankInfo},
   MasterPID ! {process_customer, self(), Msg}, % Include self() in the message
   receive
     {completed, CustomerID} ->
-      io:fwrite("Customer ~s [~p] completed.~n", [Name, CustomerID]),
-      ok
+      io:fwrite("Customer ~s [~p] completed.~n", [Name, CustomerID])
   end.
 
 
@@ -30,3 +25,16 @@ select_random_bank(BankIDs) ->
   RandomIndex = rand:uniform(length(BankIDs)),
   {BankName, BankPID} = lists:nth(RandomIndex, BankIDs),
   {BankName, BankPID}.
+
+
+sleep_random_period() ->
+  RandomSleep = rand:uniform(91) + 10,  % Generate a random number between 10 and 100
+  timer:sleep(RandomSleep).
+
+
+make_request(Name, BankIDs) ->
+  sleep_random_period(),
+  RandomLoanAmount = generate_random_loan(),
+  {_, BankPID} = select_random_bank(BankIDs), % Extract BankPID from tuple
+  io:fwrite("Customer ~s making a loan request to bank ~p for ~B dollars.~n", [Name, BankPID, RandomLoanAmount]),
+  BankPID ! {loan_request, RandomLoanAmount}.
